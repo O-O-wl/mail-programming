4486e820b436c534314a70b033c581cec243ed07
부엉이 <o.o.wl.soft@gmail.com>
2019-07-08 08:43:43 +0900

    feat: Start 20190707

---
+++ 20190707/20190707/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  20190707
+//
+//  Created by 이동영 on 08/07/2019.
+//  Copyright © 2019 부엉이. All rights reserved.
+//
+
+import Foundation
+
+func solution(array: [Int]) -> [Int] {
+    return []
+}
---
+++ 20190707/SolutionTests/SolutionTests.swift
@@ -0,0 +1,13 @@
+//
+//  SolutionTests.swift
+//  SolutionTests
+//
+//  Created by 이동영 on 08/07/2019.
+//  Copyright © 2019 부엉이. All rights reserved.
+//
+
+import XCTest
+
+class SolutionTests: XCTestCase {
+
+}
